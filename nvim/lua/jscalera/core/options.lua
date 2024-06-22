f7fb19545684d7fa89f17d4536798cf40dc7be1b
scalerjo <joseph.scalera1@gmail.com>
2024-06-21 21:56:44 -0500

    add vbg and updated options

--- nvim/lua/jscalera/core/options.lua
+++ nvim/lua/jscalera/core/options.lua
@@ -5,17 +5,18 @@ local opt = vim.opt
 opt.relativenumber = true
 opt.number = true
 
+
 -- tab & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
+opt.tabstop = 2       -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2    -- 2 spaces for indent width
+opt.expandtab = true  -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 opt.wrap = false
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if mixed case in search, assumes case-sensitive
+opt.smartcase = true  -- if mixed case in search, assumes case-sensitive
 
 opt.cursorline = true
 
@@ -23,12 +24,23 @@ opt.cursorline = true
 -- only works with true color terminals
 opt.termguicolors = true
 opt.background = "dark" -- default to dark if possible
-opt.signcolumn = "yes" -- so text doesnt shift
+opt.signcolumn = "yes"  -- so text doesnt shift
 
 opt.backspace = "indent,eol,start"
 
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+opt.splitright = true               -- split vertical window to the right
+opt.splitbelow = true               -- split horizontal window to the bottom
 
+-- Decrease mapped sequence wait time
+-- Displays which-key popup faster
+opt.timeoutlen = 300
+
+-- Controls when swap files are created
+-- also CursorHold and CursorHoldI events triggered times
+opt.updatetime = 500
+
+
+-- keep 10 lines above/below cursor when scrolling
+opt.scrolloff = 10
